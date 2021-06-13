f9eb0b0a912f3c64c4f20fbfde46ff13fc3ee29f
Radek Augustýn <raugustyn@post.cz>
2021-06-13 19:45:14 +0200

    Styling routing links...

--- src/Counters/RoutingInReact.js
+++ src/Counters/RoutingInReact.js
@@ -1,57 +1,58 @@
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
-import { useState } from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { useState } from "react";
+import "./Counters.css";
 
-const Header = ({ text }) => <h1>{text}</h1>
+const Header = ({ text }) => <h1>{text}</h1>;
 
 const Button = ({ label, onClick }) => (
-    <button onClick={onClick}>{label}</button>
-)
+  <button onClick={onClick}>{label}</button>
+);
 
 const Home = ({ count }) => {
-    return <Header text={count} />
-}
+  return <Header text={count} />;
+};
 
 const Controls = ({ count, decreaseCount, increaseCount }) => {
-    return (
-        <>
-            <Header text={count} />
-            <Button onClick={decreaseCount} label="-" />
-            <Button onClick={increaseCount} label="+" />
-        </>
-    )
-}
+  return (
+    <div>
+      <Header text={count} />
+      <Button onClick={decreaseCount} label="-" />
+      <Button onClick={increaseCount} label="+" />
+    </div>
+  );
+};
 
 const RoutingInReact = () => {
-    const [count, setCount] = useState(0)
-    const increaseCount = () => {
-        setCount(count + 1)
+  const [count, setCount] = useState(0);
+  const increaseCount = () => {
+    setCount(count + 1);
+  };
+  const decreaseCount = () => {
+    if (count > 0) {
+      setCount(count - 1);
     }
-    const decreaseCount = () => {
-        if (count > 0) {
-            setCount(count - 1)
-        }
-    }
+  };
 
-    return (
-        <Router>
-            <nav>
-                <Link to="/">Home</Link>
-                <Link to="/controls">Controls</Link>
-            </nav>
-            <Switch>
-                <Route path="/controls">
-                    <Controls
-                        increaseCount={increaseCount}
-                        decreaseCount={decreaseCount}
-                        count={count}
-                    />
-                </Route>
-                <Route path="/">
-                    <Home count={count} />
-                </Route>
-            </Switch>
-        </Router>
-    )
-}
+  return (
+    <Router>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/controls">Controls</Link>
+      </nav>
+      <Switch>
+        <Route path="/controls">
+          <Controls
+            increaseCount={increaseCount}
+            decreaseCount={decreaseCount}
+            count={count}
+          />
+        </Route>
+        <Route path="/">
+          <Home count={count} />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
 
-export default RoutingInReact
+export default RoutingInReact;
